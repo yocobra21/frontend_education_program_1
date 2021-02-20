bd14ac485e65b5d06912ff7ddc2d314239db90c4
cobra21 <artur.zayczev18@gmail.com>
2021-02-20 08:00:25 +0500

    block likebtn

--- src/index.js
+++ src/index.js
@@ -2,10 +2,5 @@ import * as $ from 'jquery';
 import style from './sass/main.sass';
 import pug from './pages/index.pug';
 
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
-
-import blocks from './blocks/blocks'
+import blocks from './blocks/blocks';
 
