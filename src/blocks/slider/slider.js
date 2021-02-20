a3b9e1895493c8e2977554525b8a76c45750920e
cobra21 <artur.zayczev18@gmail.com>
2021-02-20 17:27:09 +0500

    create slider

--- src/blocks/slider/slider.js
+++ src/blocks/slider/slider.js
@@ -2,14 +2,19 @@ import * as $ from 'jquery';
 
 $('.slider__item').ionRangeSlider({
     type: "double",
-    
     min: 0,
-    max: 1000,
-    from: 200,
-    to: 800,
+    max: 20000,
+    from: 5000,
+    to: 10000,
     grid: false,
-    // force_edges: false,     // force UI in the box
     hide_min_max: true,    // show/hide MIN and MAX labels
     hide_from_to: true,    // show/hide FROM and TO labels
-    // block: false
+    onChange: function (data) {
+        $('.slider__amount-min').text(`${data.from}₽ - `)
+        $('.slider__amount-max').text(`${data.to}₽`)
+    },
+    onStart: function (data) {
+        $('.slider__amount-min').text(`${data.from}₽ - `)
+        $('.slider__amount-max').text(`${data.to}₽`)
+    }
 })
